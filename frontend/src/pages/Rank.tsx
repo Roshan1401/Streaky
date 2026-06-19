c306c923dcf0866ecdd9dfd9edc65fb15f12218c
Roshan Patil <patilroshan9687@gmail.com>
2026-06-19 12:57:23 +0530

    feat: enhance Rank component with country, state, and city selection using react-select

--- frontend/src/pages/Rank.tsx
+++ frontend/src/pages/Rank.tsx
@@ -1,15 +1,54 @@
 import { useState } from "react";
 import { FloatingTabs } from "../components/Rank/FloatingTabs";
 import { Globe, MapPin } from "lucide-react";
+import image from "../assets/image.png";
+import { Country, State, City } from "country-state-city";
+import Select from "react-select";
+import type { SelectOption } from "../types/types";
+
+const globalCountryOptions: SelectOption[] = [
+  { label: "All Countries", value: "all" },
+  ...Country.getAllCountries().map((country) => ({
+    label: country.name,
+    value: country.isoCode,
+  })),
+];
 
 function Rank() {
-  const [rankType, setRankType] = useState("global");
+  const [mode, setMode] = useState("global");
+  const [selectedGlobalCountry, setSelectedGlobalCountry] =
+    useState<string>("all");
+  const [selectedCountry, setSelectedCountry] = useState<string>("In");
+  const [selectedState, setSelectedState] = useState<string | null>(null);
+  const [selectedCity, setSelectedCity] = useState<string | null>(null);
   const periodOptions = [
     { value: "allTime", label: "All Time" },
     { value: "today", label: "Today" },
     { value: "thisWeek", label: "This Week" },
     { value: "thisMonth", label: "This Month" },
   ];
+
+  const countryOptions: SelectOption[] = Country.getAllCountries().map(
+    (country) => ({
+      label: country.name,
+      value: country.isoCode,
+    }),
+  );
+
+  const stateOptions: SelectOption[] = selectedCountry
+    ? State.getStatesOfCountry(selectedCountry).map((state) => ({
+        label: state.name,
+        value: state.isoCode,
+      }))
+    : [];
+
+  const cityOptions: SelectOption[] = selectedState
+    ? City.getCitiesOfState(selectedCountry, selectedState).map((city) => ({
+        label: city.name,
+        value: city.name,
+      }))
+    : [];
+
   return (
     <div className="flex flex-col gap-4 px-5 py-10 xl:px-15">
       <div className="flex items-center justify-between border-b border-zinc-300 pb-4 dark:border-zinc-700">
@@ -28,7 +67,7 @@ function Rank() {
               { value: "global", label: "Global", icon: <Globe /> },
               { value: "region", label: "Region", icon: <MapPin /> },
             ]}
-            onChange={(value) => setRankType(value)}
+            onChange={(value) => setMode(value)}
             defaultValue="global"
             className=""
           />
@@ -59,6 +98,89 @@ function Rank() {
               />
             </svg>
           </div>
+        </div>
+      </div>
+
+      <div className="h-50 w-full rounded-md border border-zinc-300 dark:border-zinc-700 dark:bg-zinc-900">
+        <div className="flex w-full items-center justify-between gap-4 border-b border-zinc-300 px-5 py-3 dark:border-x-zinc-700">
+          {mode === "global" ? (
+            <div>
+              <label className="block text-sm font-medium text-(--color-text-secondary) sm:mb-2">
+                Country
+              </label>
+              <Select
+                options={globalCountryOptions}
+                value={globalCountryOptions.find(
+                  (option) => option.value === selectedGlobalCountry,
+                )}
+                onChange={(option) =>
+                  setSelectedGlobalCountry(option ? option.value : "all")
+                }
+                className="w-48"
+                classNamePrefix="select"
+                placeholder="Select Country"
+              />
+            </div>
+          ) : (
+            <div className="flex gap-4">
+              <div>
+                <label className="block text-sm font-medium text-(--color-text-secondary) sm:mb-2">
+                  Country
+                </label>
+                <Select
+                  options={countryOptions}
+                  value={countryOptions.find(
+                    (option) => option.value === selectedCountry,
+                  )}
+                  onChange={(option) =>
+                    setSelectedCountry(option ? option.value : "")
+                  }
+                  className="w-48"
+                  classNamePrefix="select"
+                  placeholder="Select Country"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-(--color-text-secondary) sm:mb-2">
+                  State
+                </label>
+                <Select
+                  options={stateOptions}
+                  value={stateOptions.find(
+                    (option) => option.value === selectedState,
+                  )}
+                  onChange={(option) =>
+                    setSelectedState(option ? option.value : "")
+                  }
+                  className="w-48"
+                  classNamePrefix="select"
+                  placeholder="Select State"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-(--color-text-secondary) sm:mb-2">
+                  City
+                </label>
+                <Select
+                  options={cityOptions}
+                  value={cityOptions.find(
+                    (option) => option.value === selectedCity,
+                  )}
+                  onChange={(option) =>
+                    setSelectedCity(option ? option.value : "")
+                  }
+                  className="w-48"
+                  classNamePrefix="select"
+                  placeholder="Select State"
+                />
+              </div>
+            </div>
+          )}
+          <img
+            src={image}
+            className="size-15 rounded-full border border-black"
+            alt="Rank"
+          />
         </div>
       </div>
     </div>
