1b7b298d680bf6f3a6fb9cdba600801d8b801638
Roshan Patil <patilroshan9687@gmail.com>
2026-07-10 00:58:34 +0530

    fix: update version to 0.0.8 in package.json and package-lock.json, and change leaderboard URL to production

--- extension/streaky/src/extension.ts
+++ extension/streaky/src/extension.ts
@@ -50,7 +50,7 @@ export async function activate(context: vscode.ExtensionContext) {
         async (message) => {
           if (message.command === "openDashboard") {
             await vscode.env.openExternal(
-              vscode.Uri.parse("https://frontend-xxx.vercel.app/leaderboard"),
+              vscode.Uri.parse("https://streaky-tawny.vercel.app/leaderboard"),
             );
 
             return;
